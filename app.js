052f2fdf7c8a1c85775a847c9f6d46164862b18d
adam <adamfravel@gmail.com>
2023-01-26 13:53:16 -0600

    added REST apis

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/campfinder", {});
@@ -16,33 +17,45 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// GET home
 app.get("/", (req, res) => {
   res.render("home");
 });
+
+// GET campgrounds
 app.get("/campgrounds", async (req, res) => {
   const campgrounds = await Campground.find({});
   res.render("campgrounds/index", { campgrounds });
 });
+
+// GET campgrounds/new
 app.get("/campgrounds/new", (req, res) => {
   res.render("campgrounds/new");
 });
 
+// POST campgrounds
 app.post("/campgrounds", async (req, res) => {
   const campground = new Campground(req.body.campground);
   await campground.save();
   res.redirect(`/campgrounds/${campground._id}`);
 });
 
+// GET campgrounds/show
 app.get("/campgrounds/:id", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/show", { campground });
 });
 
+// GET ?_method=PUT campgrounds/:id/edit
 app.get("/campgrounds/:id/edit", async (req, res) => {
   const campground = await Campground.findById(req.params.id);
   res.render("campgrounds/edit", { campground });
 });
 
+// PUT campgrounds/:id
 app.put("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findByIdAndUpdate(id, {
@@ -51,6 +64,7 @@ app.put("/campgrounds/:id", async (req, res) => {
   res.redirect(`/campgrounds/${campground._id}`);
 });
 
+// DELETE campgrounds/:id
 app.delete("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   await Campground.findByIdAndDelete(id);
