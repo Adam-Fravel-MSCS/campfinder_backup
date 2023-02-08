f840f9f4629e34e0a49062829ef2efe514fe95cd
adam <adamfravel@gmail.com>
2023-02-08 10:34:52 -0600

    fixed typo in footer

--- seeds/index.js
+++ seeds/index.js
@@ -23,12 +23,26 @@ const seedDB = async () => {
       author: "63e179a6e02f6abffb1c863b",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      // image: "https://source.unsplash.com/random/300x300?camping,${i}",
-      image: "https://source.unsplash.com/collection/483251",
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/doh0oy2mj/image/upload/v1675871052/CampFinder/osdujnkip28nnkd5shdb.jpg",
+          filename: "CampFinder/osdujnkip28nnkd5shdb",
+        },
+        {
+          url: "https://res.cloudinary.com/doh0oy2mj/image/upload/v1675871051/CampFinder/v1p6ol2lux4kqevw4ham.jpg",
+          filename: "CampFinder/v1p6ol2lux4kqevw4ham",
+        },
+      ],
     });
+    //   // image: "https://source.unsplash.com/random/300x300?camping,${i}",
+    //   image: "https://source.unsplash.com/collection/483251",
+    //   description:
+    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
+    //   price,
+    // });
     await camp.save();
   }
 };
