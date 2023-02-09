bb8148b713b2537f11808a738e0b4c4190b00379
adam <adamfravel@gmail.com>
2023-02-09 11:28:39 -0600

    added custom geolocation to seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding");
+const mapBoxToken = process.env.MAPBOX_TOKEN;
+const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
 const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
@@ -16,34 +22,57 @@ db.once("open", () => {
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-  await Campground.deleteMany({});
+  await Campground.deleteMany({}); // delete everything
+  // seed 50 new campgrounds
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    // custom
+    const name = `${sample(descriptors)} ${sample(places)}`;
     const price = Math.floor(Math.random() * 20) + 10;
+    // custom
+    const location = `${cities[random1000].city}, ${cities[random1000].state}`;
+    const geoData = await geocoder
+      .forwardGeocode({
+        query: location,
+        limit: 1,
+      })
+      .send();
+    // end custom
     const camp = new Campground({
+      // My User Id from MongoDB
       author: "63e179a6e02f6abffb1c863b",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       description:
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
       price,
+      // geometry: geoData.body.features[0].geometry,
+      geometry: {
+        type: "Point",
+        coordinates: [
+          cities[random1000].longitude,
+          cities[random1000].latitude,
+        ],
+      },
+      // geometry: {
+      //   type: "Point",
+      //   coordinates: [-113.1331, 47.0202],
+      // },
       images: [
         {
-          url: "https://res.cloudinary.com/doh0oy2mj/image/upload/v1675871052/CampFinder/osdujnkip28nnkd5shdb.jpg",
-          filename: "CampFinder/osdujnkip28nnkd5shdb",
+          // url: "https://res.cloudinary.com/doh0oy2mj/image/upload/v1675871052/CampFinder/osdujnkip28nnkd5shdb.jpg",
+          url: "https://source.unsplash.com/random/500x300?campground%20camping",
+          // filename: "CampFinder/osdujnkip28nnkd5shdb",
+          filename: "unsplash.com",
         },
         {
-          url: "https://res.cloudinary.com/doh0oy2mj/image/upload/v1675871051/CampFinder/v1p6ol2lux4kqevw4ham.jpg",
-          filename: "CampFinder/v1p6ol2lux4kqevw4ham",
+          // url: "https://res.cloudinary.com/doh0oy2mj/image/upload/v1675871051/CampFinder/v1p6ol2lux4kqevw4ham.jpg",
+          url: "https://source.unsplash.com/random/500x300?campground",
+          // filename: "CampFinder/v1p6ol2lux4kqevw4ham",
+          filename: "unsplash.com",
         },
       ],
     });
-    //   // image: "https://source.unsplash.com/random/300x300?camping,${i}",
-    //   image: "https://source.unsplash.com/collection/483251",
-    //   description:
-    //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!",
-    //   price,
-    // });
     await camp.save();
   }
 };
