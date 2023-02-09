cee51de1c836e41e536f6431fe0a89175a2b9b04
adam <adamfravel@gmail.com>
2023-02-09 13:45:21 -0600

    removed /fakeUser route

--- app.js
+++ app.js
@@ -63,12 +63,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/fakeUser", async (req, res) => {
-  const user = new User({ email: "adam@gmail.com", username: "adam" });
-  const newUser = await User.register(user, "dog");
-  res.send(newUser);
-});
-
 app.use("/", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
