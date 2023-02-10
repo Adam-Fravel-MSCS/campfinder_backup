f9706d33da3d98efe7eca0a74f0715e9267f1b90
adam <adamfravel@gmail.com>
2023-02-10 14:01:24 -0600

    updated seedcount to 10

--- seeds/index.js
+++ seeds/index.js
@@ -23,8 +23,8 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({}); // delete everything
-  // seed 300 new campgrounds
-  for (let i = 0; i < 300; i++) {
+  // seed 10 new campgrounds
+  for (let i = 0; i < 10; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     // custom
     const name = `${sample(descriptors)} ${sample(places)}`;
